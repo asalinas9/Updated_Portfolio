01b91c2f12755290248bcd5ebe9bb44316dea0ad
asalinas9 <salinas99@live.com>
2020-03-28 16:02:46 -0500

    added js, burger menu

---
+++ assets/javascript/main.js
@@ -0,0 +1,31 @@
+//select DOM items
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.menu');
+var menuNav = document.querySelector('.menu-nav');
+var menuBranding = document.querySelector('.menu-branding');
+var navItems = document.querySelectorAll('.nav-item');
+
+//set intial state of Menu
+var showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+	if (!showMenu) {
+		menuBtn.classList.add('close');
+		menu.classList.add('show');
+		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
+		navItems.forEach(item => item.classList.ad('show'));
+
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove('close');
+		menu.classList.remove('show');
+		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+		navItems.forEach(item => item.classList.remove('show'));
+
+		showMenu = false;
+	}
+}
